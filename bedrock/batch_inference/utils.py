1dd6a5d8aed710d484da2e32bda2b2ad21819000
iizus <iizus@amazon.co.jp>
2024-02-08 02:49:04 +0000

    update

--- bedrock/batch_inference/utils.py
+++ bedrock/batch_inference/utils.py
@@ -6,3 +6,10 @@ def get_formatted_time() -> str:
     jst = timezone(time_delta)
     formatted_time = now.astimezone(jst).strftime("%Y%m%d-%H%M%S")
     return formatted_time
+
+
+from yaml import safe_load
+
+def load_config(file_name:str="config.yaml") -> dict:
+    with open(file_name) as config_file:
+        return safe_load(config_file)
