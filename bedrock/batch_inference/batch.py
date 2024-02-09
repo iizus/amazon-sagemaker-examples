7f28697fccf55ed2f9ce53fa16f25671369c7ab4
iizus <iizus@amazon.co.jp>
2024-02-09 12:38:14 +0000

    update

--- bedrock/batch_inference/batch.py
+++ bedrock/batch_inference/batch.py
@@ -20,46 +20,45 @@ class Batch:
         return self.__upload(inputs)
 
 
-    def create_job(self, condition:str, output_dir:str) -> dict:
-        response:dict = self.__create_by(condition, output_dir)
-        self.arn = response.get("jobArn")
-        self.id = self.arn.split("/")[-1].strip()
+    def create_job(self, job_name:str, output_dir:str) -> dict:
+        response:dict = self.__create(job_name, output_dir)
+        self.arn = response.get('jobArn')
+        self.id = self.arn.split('/')[-1].strip()
         self.get_job_info()
         return response
 
 
     def __upload(self, inputs:str) -> (str, str):
-        condition:str = f"{self.model_id}/{inputs.count('recordId')}"
-        output_dir:str = f"Bedrock/Batch-Inference/{condition}"
+        job_name:str = f"{self.model_id}/{inputs.count('recordId')}"
+        output_dir:str = f"Bedrock/Batch-Inference/{job_name}"
         input_key:str = f"{_output_dir}/{jsonl_file_name}"
         input_object = bucket.Object(key=_input_key)
         response:dict = input_object.put(Body=inputs)
-        return condition, output_dir
+        return job_name, output_dir
 
 
-    def __create_by(self, condition:str, output_dir:str) -> dict:
-        _output_dir:str = f"s3://{bucket_name}/{output_dir}/"
+    def __create(self, job_name:str, output_dir:str) -> dict:
         return self.__bedrock.create_job(
             model_id = self.model_id,
-            job_name = condition,
-            input_key = f"{_output_dir}{jsonl_file_name}",
+            job_name = job_name,
+            input_key = f"s3://{bucket_name}/{output_dir}/{jsonl_file_name}",
             output_dir = output_dir,
         )
 
 
     def get_status(self) -> str:
         job_info:dict = self.get_job_info()
-        return job_info.get("status")
+        return job_info.get('status')
 
 
     def get_job_info(self) -> dict:
         job_info:dict = self.__bedrock.get_job_info(job_arn=self.arn)
         
-        self.name:str = job_info.get("jobName")
-        self.status:str = job_info.get("status")
-        self.error:str = job_info.get("message")
-        self.submit_time:datetime = job_info.get("submitTime")
-        self.last_modified_time:datetime = job_info.get("lastModifiedTime")
+        self.name:str = job_info.get('jobName')
+        self.status:str = job_info.get('status')
+        self.error:str = job_info.get('message')
+        self.submit_time:datetime = job_info.get('submitTime')
+        self.last_modified_time:datetime = job_info.get('lastModifiedTime')
         
         self.progress_time:delta = self.last_modified_time - self.submit_time
 
